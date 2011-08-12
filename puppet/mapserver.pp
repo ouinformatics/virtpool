2adf9a405dade3b4f3d1bb6a3eb2e3a4e3147669
Jonah Duckles <jduckles@ou.edu>
2011-08-12 08:42:58 -0500

    Comments and typo fixed.

--- puppet/mapserver.pp
+++ puppet/mapserver.pp
@@ -1,25 +1,35 @@
 $apache_home = '/scratch/www/html'
+$data_home = '/scratch/data/'
+
+package { 'httpd':
+            ensure => present,
+}
 
 package { 'mapserver6':
 	ensure => present,
 }
 
-package { 'mod-fcgid':
+package { 'mod_fcgid':
          ensure => present,
 }
 
-
+# Set up cgi-bin with normal CGI
 file { "${apache_home}/cgi-bin/mapserv":
         source => '/usr/libexec/mapserv',
         ensure => file,
         mode => 755,
 }
 
+# Setup fastCGI as well
 file { '/scratch/www/html/cgi-bin/mapserv.fcgi':
         source => '/usr/libexec/mapserv',
         ensure => file,
         mode => 755,
 }
+
+
+# Get mapfiles in place
+
 
 
 
