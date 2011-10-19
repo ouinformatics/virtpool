b4e21e92821cb4bab1a2018f9322a6de428c0a23
Jonah Duckles <jduckles@ou.edu>
2011-10-19 15:09:47 -0500

    Fixed url and sudo

--- config/celeryd/install_celery.sh
+++ config/celeryd/install_celery.sh
@@ -1,7 +1,7 @@
 # Installing celery worker
 sudo /usr/bin/easy_install-2.6 pip
 sudo /usr/bin/pip install -U Celery pymongo psycopg
-wget http://github.com/ouinformatics/  -O /etc/init.d/celeryd
+sudo wget --no-check-certificate http://github.com/ouinformatics/virtpool/raw/master/config/celeryd/celeryd  -O /etc/init.d/celeryd
 sudo chmod +x /etc/init.d/celeryd
 sudo /sbin/chkconfig --add /etc/init.d/celeryd
 sudo mkdir /opt/celeryq
