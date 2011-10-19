4f7a5dcccff572bed4a4f7479371ca7fb46da249
Jonah Duckles <jonahd@gmail.com>
2011-10-19 15:11:02 -0500

    Note on fabric adaptation.

--- config/celeryd/install_celery.sh
+++ config/celeryd/install_celery.sh
@@ -1,4 +1,5 @@
 # Installing celery worker
+# Should convert this script to a fabric script
 sudo /usr/bin/easy_install-2.6 pip
 sudo /usr/bin/pip install -U Celery pymongo psycopg
 sudo wget --no-check-certificate http://github.com/ouinformatics/virtpool/raw/master/config/celeryd/celeryd  -O /etc/init.d/celeryd
