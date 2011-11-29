d7d10410ca1b5f5b0094c80b6e9cfdbfb03bbf87
Jonah Duckles <jonahd@gmail.com>
2011-11-29 13:05:52 -0600

    Few changes for condor vms.

--- config/celeryd/install_celery.sh
+++ config/celeryd/install_celery.sh
@@ -1,9 +1,11 @@
 # Installing celery worker
 # Should convert this script to a fabric script
-sudo /usr/bin/easy_install-2.6 pip
-sudo /usr/bin/pip install -U Celery pymongo psycopg
+sudo apt-get install python-virtualenv python-psycopg2 python-pymongo
 sudo wget --no-check-certificate http://github.com/ouinformatics/virtpool/raw/master/config/celeryd/celeryd  -O /etc/init.d/celeryd
 sudo chmod +x /etc/init.d/celeryd
 sudo /sbin/chkconfig --add /etc/init.d/celeryd
 sudo mkdir /opt/celeryq
+cd /opt/celeryq
+sudo ./virtpy/bin/pip install Celery
+
 # Download queue's worker code
